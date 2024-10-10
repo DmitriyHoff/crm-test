0b4ca34ec2065fee4214d5f429abf4fb724d96b1
Dmitriy Hoff <goffdmitriy@gmail.com>
2024-10-11 01:44:52 +0200

    update

--- src/controllers/companies.controller.ts
+++ src/controllers/companies.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Post, Body, Res, HttpException, HttpStatus } from '@nestjs/common';
-import { AppService, Company } from '../app.service';
-import { InstanceAxios } from 'src/axiosInstance';
+import { Controller, Post, Body, Res } from '@nestjs/common';
+import { AppService, Company, EntityType } from '../app.service';
 import type { Response } from 'express'
 
 @Controller('companies')
@@ -9,29 +8,6 @@ export class CompaniesController {
 
     @Post()
     async createCompany(@Body() companies: Array<Company>, @Res({ passthrough: true }) res: Response): Promise<string> {
-        try {
-            // отправляем полученные сделки
-            const response = await InstanceAxios.instance
-                .post('/api/v4/contacts', companies);
-
-            // полученный статус устанавливаем как статус ответа
-            // и отправляем полученные данные
-            res.status(response.status)
-            return response.data;
-        } catch (error) {
-            // в случае, если статус не 200
-            if(error.response) {
-                // полученный статус устанавливаем как статус ответа
-                // и отправляем полученные данные
-                res.status(error.response.status)
-                return error.response.data;
-            }
-            // в остальных случаях отправим статус 500
-            console.log(error);
-            throw new HttpException(
-                'Internal Server error',
-                HttpStatus.INTERNAL_SERVER_ERROR,
-            );
-        }
+        return await this.appService.createEntity(EntityType.Companies, res, companies);
     }
 }
--- src/controllers/leads.controller.ts
+++ src/controllers/leads.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Post, Body, Res, HttpException, HttpStatus } from '@nestjs/common';
-import { AppService, Lead } from '../app.service';
-import { InstanceAxios } from 'src/axiosInstance';
+import { Controller, Post, Body, Res } from '@nestjs/common';
+import { AppService, EntityType, Lead } from '../app.service';
 import type { Response } from 'express'
 
 @Controller('leads')
@@ -9,30 +8,7 @@ export class LeadsController {
 
     @Post()
     async createLead(@Body() leads: Array<Lead>, @Res({ passthrough: true }) res: Response): Promise<string> {
-        try {
-            // отправляем полученные сделки
-            const response = await InstanceAxios.instance
-                .post('/api/v4/leads', leads);
-
-            // полученный статус устанавливаем как статус ответа
-            // и отправляем полученные данные
-            res.status(response.status)
-            return response.data;
-        } catch (error) {
-            // в случае, если статус не 200
-            if(error.response) {
-                // полученный статус устанавливаем как статус ответа
-                // и отправляем полученные данные
-                res.status(error.response.status)
-                return error.response.data;
-            }
-            // в остальных случаях отправим статус 500
-            console.log(error);
-            throw new HttpException(
-                'Internal Server error',
-                HttpStatus.INTERNAL_SERVER_ERROR,
-            );
-        }
+        return await this.appService.createEntity(EntityType.Leads, res, leads);
     }
 }
 
